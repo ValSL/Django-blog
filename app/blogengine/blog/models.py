0ee1930d0fb0ce30b39c50a433d55cfbfef51534
valid <valid.saley@gmail.com>
2020-01-10 21:48:57 +0300

    add get_absolute_url

--- app/blogengine/blog/models.py
+++ app/blogengine/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
 
@@ -8,6 +9,9 @@ class Post(models.Model):
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)# blank=True означает что это поле модет быть пустым
     date_pub = models.DateTimeField(auto_now_add=True)# auto_now_add=True значит будт заполнятся при сохранении в БД
+
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return f'{self.title}'
