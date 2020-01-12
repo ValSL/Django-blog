1582ad40489635b8756af0998f1c696d4b8be0d7
valid <valid.saley@gmail.com>
2020-01-12 12:36:37 +0300

    second try forms

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -4,7 +4,8 @@ from django.views.generic import View
 from .models import Post, Tag
 from django.shortcuts import get_object_or_404
 from .utils import ObjectDetailMixin
-
+from .forms import TagForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -39,6 +40,20 @@ class TagDetail(ObjectDetailMixin, View):
     #     # tag = Tag.objects.get(slug__iexact=slug)
     #     tag = get_object_or_404(Tag, slug__iexact=slug)
     #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
 
 
 def tags_list(request):
