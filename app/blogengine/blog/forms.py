052c16f84684b873d2f7e14e38ac2027653be5fa
valid <valid.saley@gmail.com>
2020-01-12 14:20:20 +0300

    class PostForm

--- app/blogengine/blog/forms.py
+++ app/blogengine/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Tag
+from .models import Tag, Post
 from django.core.exceptions import ValidationError
 
 
@@ -18,7 +18,7 @@ class TagForm(forms.ModelForm):
             'slug': forms.TextInput(attrs={'class': 'form-control'}),
         }
 
-    def clean_slug(self):
+    def clean_slug(self): # Валидатор для добавления в словарь cleaned_data (clean_<-поле формы->) соглашение Django
         new_slug = self.cleaned_data['slug'].lower()
         if new_slug == 'create':
             raise ValidationError('Slug may not be "create"')
@@ -29,3 +29,23 @@ class TagForm(forms.ModelForm):
     # def save(self):
     #     new_tag = Tag.objects.create(title=self.cleaned_data['title'], slug=self.cleaned_data['slug'])
     #     return new_tag
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'tags']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "create"')
+        return new_slug
